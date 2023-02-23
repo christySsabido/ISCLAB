c0aeee784efb1602a87ff4235b2b34425e550902
christySsabido <Christy.19070055@itsmotul.edu.mx>
2023-02-22 23:14:36 -0600

    Prototipo

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import abajo from './components/abajo.vue'
+
+</script>
+
+<template>
+  <header class="bg-sky-600">
+
+    <div class="flex flex-wrap justify-center ">
+  <img
+        src="./assets/ISCLAB.png"
+        class="w-2/3"
+        alt=""
+      />
+     
+</div>
+
+
+    <div class="wrapper">
+      <HelloWorld />
+    </div>
+  </header>
+
+  <main class="flex bg-cyan-200">
+    <TheWelcome />
+  </main>
+
+  <aside class="">
+    <abajo/>
+  </aside>
+
+  <div class="">
+    <Division/>
+  </div>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+.medio {
+  margin-top: 60 px;
+}
+
+div .titulo1 {
+  margin-top: 60 px;
+}
+
+
+
+</style>
---
+++ src/components/abajo.vue
@@ -0,0 +1,24 @@
+<template>
+  <WelcomeItem>
+    <template>
+      <DocumentationIcon />
+  
+    </template>
+  <div class="bg-teal-100">
+
+    <div class=" titulo1 flex ">
+    <div class="texto w-1/2  text-xl text-left whitespace-normal" style="margin: 100px 500px 100px 50px;"> Cualquier tipo de proyecto es bueno siempre y cuando cumpla con los estandares y recomendaciones apegadas en la carrera.
+<br>
+<br>
+      Los proyectos pueden ser una pagina web e incluso un software que satisfaga necesidaes segun sea tu idea en mente.
+    </div>
+
+    <div class="intro text-6xl w-1/2 font-mono font-family: SFMono-Regular font-bold" style="margin: 100px 50px 0px 50px; font-style: italic;">¿Podemos proponer cualquier tipo de proyecto?</div>
+
+
+    
+  </div>
+  </div>
+  </WelcomeItem>
+
+  </template>
